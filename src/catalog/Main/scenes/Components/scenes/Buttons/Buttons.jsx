30db18f5b7aeeeb1ee4fb69fc4af5d4c170e3c24
Marina Blin <marina.blin@zenika.com>
2019-05-17 11:44:44 +0200

    :sparkles: Start create button

--- src/catalog/Main/scenes/Components/scenes/Buttons/Buttons.jsx
+++ src/catalog/Main/scenes/Components/scenes/Buttons/Buttons.jsx
@@ -1,5 +1,42 @@
 import React from 'react'
+import {Sandbox, withDependencies} from 'react-sandbox-editor'
+import '../../../../../../library/buttons.scss'
 
-const Buttons = () => <h2>Buttons</h2>
+const ReactSandbox = withDependencies([
+    'https://unpkg.com/react@16.6.0/umd/react.development.js',
+    'https://unpkg.com/react-dom@16.6.0/umd/react-dom.development.js'
+  ])(Sandbox)
+
+const Buttons = () => <ReactSandbox
+  classes={{
+    header: undefined
+  }}
+  dependencies={['http://localhost:3000/evoli.css']}
+  displayMode="horizontal-split"
+  executeOnCodeChange
+  executeOnCodeChangeDebounce={1000}
+  horizontalSplitOffset={20}
+  onDisplayModeButtonClick={function(){}}
+  permissions={[
+    'allow-forms',
+    'allow-pointer-lock',
+    'allow-popups',
+    'allow-modals',
+    'allow-same-origin',
+    'allow-scripts',
+    'allow-top-navigation'
+  ]}
+  templateEditor={{
+    defaultValue: 
+                '<div>\n' +
+                '   <button className="btn-primary">Valider</button>\n' +
+                '</div>',
+    mode: 'html'
+  }}
+  scriptEditor={{
+    mode: 'javascript'
+  }}
+  theme="tomorrow"
+/>
 
 export default Buttons
