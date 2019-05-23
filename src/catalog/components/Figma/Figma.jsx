fd2a0cba474416f65429cbdbdad5732127e3bcbc
Marina Blin <marina.blin@zenika.com>
2019-05-23 17:54:58 +0200

    :sparkles: Added bulma dependancie

--- src/catalog/components/Figma/Figma.jsx
+++ src/catalog/components/Figma/Figma.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
+import './Figma.css'
+
 const Figma = ({ nodeId }) => (
-  <>
+  <div>
     <iframe
+      className="figma"
       id={'id-' + nodeId}
       title={nodeId}
-      width="800"
-      height="450"
       src={
         'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FFNTIGdJacxKH3DHRkomttIFv%2FEvoliCSS%3Fnode-id%3D' +
         nodeId
       }
     />
-  </>
+  </div>
 )
 
 export default Figma
