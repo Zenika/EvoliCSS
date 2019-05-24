08323f069fefc5a53e69d720fd69b312bf08c1de
Bugsyaya <marina.blin@zenika.com>
2019-05-24 16:06:44 +0200

    :sparkles: Added code element/box

--- src/catalog/Main/scenes/Element/scenes/Box/Box.jsx
+++ src/catalog/Main/scenes/Element/scenes/Box/Box.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Figma from 'catalog/components/Figma'
+import BoxGeneral from './BoxGeneral/BoxGeneral'
 
 const Box = () => (
   <div>
     <h2>Box</h2>
     <Figma nodeId="4%253A160" />
+    <BoxGeneral />
   </div>
 )
 
