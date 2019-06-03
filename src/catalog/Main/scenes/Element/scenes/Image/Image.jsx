38159218774bcda78785331cc095f34c38d3494a
Marina Blin <marina.blin@zenika.com>
2019-06-03 16:41:29 +0200

    :sparkles: Added element Image

--- src/catalog/Main/scenes/Element/scenes/Image/Image.jsx
+++ src/catalog/Main/scenes/Element/scenes/Image/Image.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Figma from 'catalog/components/Figma'
+import ImageNormal from './ImageNormal/ImageNormal'
+import ImageSquare from './ImageSquare/ImageSquare'
+import ImageRounded from './ImageRounded/ImageRounded'
 
 const Image = () => (
   <div>
     <h2>Image</h2>
     <Figma nodeId="4%253A162" />
+    <ImageNormal />
+    <ImageSquare />
+    <ImageRounded />
   </div>
 )
 
---
+++ src/catalog/Main/scenes/Element/scenes/Image/ImageRounded/ImageRounded.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Sandbox from 'catalog/components/Sandbox/Sandbox'
+
+const ImageSquare = () => (
+  <div>
+    <h3>Square</h3>
+    <Sandbox>
+      <figure className="image is-128x128">
+        <img
+          alt="is-128x128"
+          className="is-rounded"
+          src="https://bulma.io/images/placeholders/128x128.png"
+        />
+      </figure>
+    </Sandbox>
+  </div>
+)
+
+export default ImageSquare
