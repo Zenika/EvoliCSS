93fe4397ab3c204f1579b0c64ad3107bcacfc87c
Marina Blin <marina.blin@zenika.com>
2019-06-03 11:23:26 +0200

    :sparkles: Added forms radio

--- src/catalog/Main/scenes/Forms/scenes/Radio/Radio.jsx
+++ src/catalog/Main/scenes/Forms/scenes/Radio/Radio.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import Figma from 'catalog/components/Figma'
+import Sandbox from 'catalog/components/Sandbox/Sandbox'
 
 const Radio = () => (
   <div>
     <h2>Radio</h2>
     <Figma nodeId="4%253A194" />
+    <Sandbox>
+      <div class="control">
+        <label class="radio">
+          <input type="radio" name="answer" />
+          Yes
+        </label>
+        <label class="radio">
+          <input type="radio" name="answer" />
+          No
+        </label>
+      </div>
+    </Sandbox>
+    <Sandbox>
+      <div class="control">
+        <label class="radio">
+          <input type="radio" name="foobar" />
+          Foo
+        </label>
+        <label class="radio">
+          <input type="radio" name="foobar" checked />
+          Bar
+        </label>
+      </div>
+    </Sandbox>
+    <Sandbox>
+      <div class="control">
+        <label class="radio">
+          <input type="radio" name="rsvp" />
+          Going
+        </label>
+        <label class="radio">
+          <input type="radio" name="rsvp" />
+          Not going
+        </label>
+        <label class="radio" disabled>
+          <input type="radio" name="rsvp" disabled />
+          Maybe
+        </label>
+      </div>
+    </Sandbox>
   </div>
 )
 
