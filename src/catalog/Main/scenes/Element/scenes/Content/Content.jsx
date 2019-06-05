70569fea2c497bb57f5d88ac42e147d9e0b345d7
Marina Blin <marina.blin@zenika.com>
2019-06-05 11:14:28 +0200

    :sparkles: Added element content

--- src/catalog/Main/scenes/Element/scenes/Content/Content.jsx
+++ src/catalog/Main/scenes/Element/scenes/Content/Content.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Figma from 'catalog/components/Figma'
+import ContentNormal from './ContentNormal/ContentNormal'
 
 const Content = () => (
   <div>
     <h2>Content</h2>
     <Figma nodeId="4%253A178" />
+    <ContentNormal />
   </div>
 )
 
