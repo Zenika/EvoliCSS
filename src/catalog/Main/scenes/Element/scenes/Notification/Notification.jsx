4dd7860b1f61cf0f3e298b7b695481e35bd83a92
Marina Blin <Bugsyaya@users.noreply.github.com>
2019-06-05 14:53:44 +0200

    :sparkles: Added element Notification (#38)

--- src/catalog/Main/scenes/Element/scenes/Notification/Notification.jsx
+++ src/catalog/Main/scenes/Element/scenes/Notification/Notification.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Figma from 'catalog/components/Figma'
+import NotificationNormal from './NotificationNormal/NotificationNormal'
+import NotificationColors from './NotificationColors/NotificationColors'
 
 const Notification = () => (
   <div>
     <h2>Notification</h2>
     <Figma nodeId="4%253A172" />
+    <NotificationNormal />
+    <NotificationColors />
   </div>
 )
 
---
+++ src/catalog/Main/scenes/Element/scenes/Notification/NotificationColors/NotificationColors.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Sandbox from 'catalog/components/Sandbox/Sandbox'
+
+const NotificationColors = () => (
+  <div>
+    <h3>Colors</h3>
+    <Sandbox>
+      <div className="notification is-primary">
+        <button className="delete" />
+        Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor.{' '}
+        <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum
+        rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum{' '}
+        <a href="https://github.com/Zenika/EvoliCSS">felis venenatis</a> efficitur. Sit amet,
+        consectetur adipiscing elit
+      </div>
+
+      <div className="notification is-link">
+        <button className="delete" />
+        Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor.{' '}
+        <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum
+        rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum{' '}
+        <a href="https://github.com/Zenika/EvoliCSS">felis venenatis</a> efficitur. Sit amet,
+        consectetur adipiscing elit
+      </div>
+
+      <div className="notification is-info">
+        <button className="delete" />
+        Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor.{' '}
+        <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum
+        rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum{' '}
+        <a href="https://github.com/Zenika/EvoliCSS">felis venenatis</a> efficitur. Sit amet,
+        consectetur adipiscing elit
+      </div>
+
+      <div className="notification is-success">
+        <button className="delete" />
+        Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor.{' '}
+        <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum
+        rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum{' '}
+        <a href="https://github.com/Zenika/EvoliCSS">felis venenatis</a> efficitur. Sit amet,
+        consectetur adipiscing elit
+      </div>
+
+      <div className="notification is-warning">
+        <button className="delete" />
+        Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor.{' '}
+        <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum
+        rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum{' '}
+        <a href="https://github.com/Zenika/EvoliCSS">felis venenatis</a> efficitur. Sit amet,
+        consectetur adipiscing elit
+      </div>
+
+      <div className="notification is-danger">
+        <button className="delete" />
+        Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor.{' '}
+        <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum
+        rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum{' '}
+        <a href="https://github.com/Zenika/EvoliCSS">felis venenatis</a> efficitur. Sit amet,
+        consectetur adipiscing elit
+      </div>
+    </Sandbox>
+  </div>
+)
+
+export default NotificationColors
