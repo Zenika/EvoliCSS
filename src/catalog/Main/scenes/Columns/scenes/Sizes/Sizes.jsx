ef0b7084751b85e8ae98a484d2b3cf1fb8720cb6
Marina Blin <marina.blin@zenika.com>
2019-06-05 11:36:59 +0200

    :sparkles: Added columns sizes

--- src/catalog/Main/scenes/Columns/scenes/Sizes/Sizes.jsx
+++ src/catalog/Main/scenes/Columns/scenes/Sizes/Sizes.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Figma from 'catalog/components/Figma'
+import SizesNormal from './SizesNormal/SizesNormal'
+import SizesSystem from './SizesSystem/SizesSystem'
+import SizesOffset from './SizesOffset/SizesOffset'
+import SizesNarrow from './SizesNarrow/SizesNarrow'
 
 const Sizes = () => (
   <div>
     <h2>Sizes</h2>
     <Figma nodeId="4%253A186" />
+    <SizesNormal />
+    <SizesSystem />
+    <SizesOffset />
+    <SizesNarrow />
   </div>
 )
 export default Sizes
---
+++ src/catalog/Main/scenes/Columns/scenes/Sizes/SizesNormal/SizesNormal.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import Sandbox from 'catalog/components/Sandbox/Sandbox'
+
+const SizesNormal = () => (
+  <div>
+    <h2>Normal</h2>
+    <Sandbox>
+      <div className="columns">
+        <div className="column is-four-fifths">is-four-fifths</div>
+        <div className="column">Auto</div>
+        <div className="column">Auto</div>
+      </div>
+
+      <div className="columns">
+        <div className="column is-three-quarters">is-three-quarters</div>
+        <div className="column">Auto</div>
+        <div className="column">Auto</div>
+      </div>
+
+      <div className="columns">
+        <div className="column is-two-thirds">is-two-thirds</div>
+        <div className="column">Auto</div>
+        <div className="column">Auto</div>
+      </div>
+
+      <div className="columns">
+        <div className="column is-three-fifths">is-three-fifths</div>
+        <div className="column">Auto</div>
+        <div className="column">Auto</div>
+      </div>
+
+      <div className="columns">
+        <div className="column is-half">is-half</div>
+        <div className="column">Auto</div>
+        <div className="column">Auto</div>
+      </div>
+
+      <div className="columns">
+        <div className="column is-two-fifths">is-two-fifths</div>
+        <div className="column">Auto</div>
+        <div className="column">Auto</div>
+      </div>
+
+      <div className="columns">
+        <div className="column is-one-third">is-one-third</div>
+        <div className="column">Auto</div>
+        <div className="column">Auto</div>
+      </div>
+
+      <div className="columns">
+        <div className="column is-one-quarter">is-one-quarter</div>
+        <div className="column">Auto</div>
+        <div className="column">Auto</div>
+      </div>
+
+      <div className="columns">
+        <div className="column is-one-fifth">is-one-fifth</div>
+        <div className="column">Auto</div>
+        <div className="column">Auto</div>
+      </div>
+    </Sandbox>
+  </div>
+)
+export default SizesNormal
