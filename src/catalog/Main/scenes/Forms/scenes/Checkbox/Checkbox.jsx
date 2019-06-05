3ce426b8412300f97efec7221709b4a8e98d5b9f
Marina Blin <Bugsyaya@users.noreply.github.com>
2019-06-05 14:52:02 +0200

    :sparkles: Added forms checkbox (#31)

--- src/catalog/Main/scenes/Forms/scenes/Checkbox/Checkbox.jsx
+++ src/catalog/Main/scenes/Forms/scenes/Checkbox/Checkbox.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Figma from 'catalog/components/Figma'
+import Sandbox from 'catalog/components/Sandbox/Sandbox'
 
 const Checkbox = () => (
   <div>
     <h2>Checkbox</h2>
     <Figma nodeId="4%253A202" />
+    <Sandbox>
+      <label className="checkbox">
+        <input type="checkbox" />
+        Remember me
+      </label>
+    </Sandbox>
+    <Sandbox>
+      <label className="checkbox">
+        <input type="checkbox" />I agree to the{' '}
+        <a href="https://github.com/Zenika/EvoliCSS">terms and conditions</a>
+      </label>
+    </Sandbox>
+    <Sandbox>
+      <label className="checkbox" disabled>
+        <input type="checkbox" disabled />
+        Save my preferences
+      </label>
+    </Sandbox>
   </div>
 )
 
