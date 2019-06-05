bbbdaca03a4156c028c01e91d786566ba6d3f028
Marina Blin <Bugsyaya@users.noreply.github.com>
2019-06-05 14:53:17 +0200

    :sparkles: Added element Title (#36)

--- src/catalog/Main/scenes/Element/scenes/Title/Title.jsx
+++ src/catalog/Main/scenes/Element/scenes/Title/Title.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Figma from 'catalog/components/Figma'
+import TitleNormal from './TitleNormal/TitleNormal'
+import TitleSizes from './TitleSizes/TitleSizes'
 
 const Title = () => (
   <div>
     <h2>Title</h2>
     <Figma nodeId="4%253A168" />
+    <TitleNormal />
+    <TitleSizes />
   </div>
 )
 
