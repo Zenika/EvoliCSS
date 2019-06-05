dd8fc038fa12cf9c7e681d43ca993859c21b79fb
Marina Blin <marina.blin@zenika.com>
2019-06-05 11:22:34 +0200

    :sparkles: Added columns basics

---
+++ src/catalog/Main/scenes/Columns/scenes/Basics/BasicsNormal/BasicsNormal.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sandbox from 'catalog/components/Sandbox/Sandbox'
+
+const BasicsNormal = () => (
+  <div>
+    <h3>Normal</h3>
+    <Sandbox>
+      <div class="columns">
+        <div class="column">First column</div>
+        <div class="column">Second column</div>
+        <div class="column">Third column</div>
+        <div class="column">Fourth column</div>
+      </div>
+    </Sandbox>
+  </div>
+)
+export default BasicsNormal
